2b0cc4f1cedd6ff6fcfe0c920bc2ffa4d92881b3
yousra <yousra@Yousra>
2017-12-06 15:24:45 +0100

    commit yousra

--- src/actions/ActionDebut.java
+++ src/actions/ActionDebut.java
@@ -1,5 +1,5 @@
 package actions;
-
+//yousrabouzid
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,6 +11,8 @@ public class ActionDebut implements Action{
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		
 		return "/Login.jsp";
+		
 	}
+
 
 }
